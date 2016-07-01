5f76c8bfaa96694a33613778ef6576a0e4e7f960
jpgreenstein <justin.greenstein@saltermitchell.com>
2016-07-01 00:41:53 -0400

    update

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('.ryu').mouseenter(function() {
     $('.ryu-still').hide();
-	$('.ryu-ready').show();
+	  $('.ryu-ready').show();
   })
   .mouseleave(function() {
     $('.ryu-ready').hide();
@@ -9,11 +9,17 @@ $(document).ready(function() {
   })
   .mousedown(function() {
     console.log('mousedown');
-    // play hadouken sound
+    playHadouken();
     $('.ryu-ready').hide();
     $('.ryu-throwing').show();
-    $('.hadouken').show();
-    // show hadouken and animate it to the right of the screen
+    $('.hadouken').finish().show().animate(
+      {'left': '1020px'},
+      500,
+      function() {
+        $(this).hide();
+        $(this).css('left', '520px');
+      }
+    );
   })
   .mouseup(function() {
     console.log('mouseup');
@@ -21,4 +27,11 @@ $(document).ready(function() {
     $('.ryu-throwing').hide();
     $('.ryu-ready').show();
   });
+
 });
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
